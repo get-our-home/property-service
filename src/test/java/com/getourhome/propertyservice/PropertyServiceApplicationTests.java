ba9df687bf05c6002033f4e224b761d1faecb075
wonjun0120 <wonjundero@gmail.com>
2024-07-16 22:50:49 +0900

    feat: docker, docker compose
    #3

--- src/test/java/com/getourhome/propertyservice/PropertyServiceApplicationTests.java
+++ src/test/java/com/getourhome/propertyservice/PropertyServiceApplicationTests.java
@@ -1,10 +1,15 @@
 package com.getourhome.propertyservice;
 
+import com.getourhome.propertyservice.util.JwtTokenProvider;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class PropertyServiceApplicationTests {
+
+    @MockBean
+    private JwtTokenProvider jwtTokenProvider;
 
     @Test
     void contextLoads() {
